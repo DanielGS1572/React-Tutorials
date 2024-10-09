5cd81eab391c35ea1b5cfe148d211fb5d9b9c81e
Daniel <gonzalisd@gmail.com>
2024-10-09 14:05:15 -0600

    React

---
+++ 1-React Fundamentals/katas/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import {MyClass} from './Components/K4';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyClass name="myNamxe"><button>Hello world</button></MyClass>
+    </div>
+  );
+}
+
+export default App;
---
+++ 1-React Fundamentals/katas/src/Components/K4.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class MyClass extends Component{
+    render(){
+        return <div>ok {this.props.name}</div>
+    }
+}
